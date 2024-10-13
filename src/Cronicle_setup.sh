9fb50e5247f9b7332e28e7d9eda6e06b2fc03a05
orztv <qi@outlook.cz>
2024-10-13 14:31:11 +0800

    pro

--- src/Cronicle_setup.sh
+++ src/Cronicle_setup.sh
@@ -3,42 +3,42 @@ set -e
 
 echo "安装Cronicle开始"
 
-cronicle_version=${CRONICLE_VER}
-cronicle_basedir=${CRONICLE_base_dir}
-
 # 安装 Cronicle
-mkdir -p ${cronicle_basedir}
-chown -R ${USER}:${USER} ${cronicle_basedir}
-cd ${cronicle_basedir}
-curl -L https://github.com/jhuckaby/Cronicle/archive/v${cronicle_version}.tar.gz | tar zxvf - --strip-components 1
-npm install --production
-node bin/build.js dist
+mkdir -p ${CRONICLE_base_dir} ${CRONICLE_base_dir}/{logs,data,plugins,conf}
+chown -R ${USER}:${USER} ${CRONICLE_base_dir}
 
 # 设置 Cronicle 配置
-cat << EOF > ${cronicle_basedir}/conf/config.json
+cat << EOF > ${CRONICLE_base_dir}/conf/config.json
 {
-  "base_app_url": "http://localhost:7863",
+  "base_app_url": "http://localhost:${CRONICLE_PORT}",
   "server_hostname": "localhost",
-  "server_port": 7863,
+  "server_port": ${CRONICLE_PORT},
   "web_socket_use_hostnames": false,
-  "base_dir": "${cronicle_basedir}",
-  "log_dir": "${cronicle_basedir}/logs",
-  "conf_dir": "${cronicle_basedir}/conf",
-  "data_dir": "${cronicle_basedir}/data",
-  "plugins_dir": "${cronicle_basedir}/plugins",
-  "pid_file": "${cronicle_basedir}/logs/cronicled.pid",
+  "base_dir": "${CRONICLE_base_dir}",
+  "log_dir": "${CRONICLE_base_dir}/logs",
+  "conf_dir": "${CRONICLE_base_dir}/conf",
+  "data_dir": "${CRONICLE_base_dir}/data",
+  "plugins_dir": "${CRONICLE_base_dir}/plugins",
+  "pid_file": "${CRONICLE_base_dir}/logs/cronicled.pid",
   "secret_key": "CHANGE_THIS",
   "Storage": {
     "engine": "Filesystem",
     "list_page_size": 50,
     "concurrency": 4,
-    "base_dir": "${cronicle_basedir}/data"
+    "base_dir": "${CRONICLE_base_dir}/data"
+  },
+  "WebServer": {
+    "http_port": ${CRONICLE_PORT}
   }
 }
 EOF
 
+cd ${CRONICLE_base_dir}
+curl -L https://github.com/jhuckaby/Cronicle/archive/v${CRONICLE_VER}.tar.gz | tar zxvf - --strip-components 1
+npm install --omit=dev
+node bin/build.js dist
+
 # 初始化存储
-cd ${cronicle_basedir}
 node bin/storage-cli.js setup
 
 # 创建管理员用户
