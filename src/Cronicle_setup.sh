6565420f9a1494a7560969a61c6f7f4797928e32
orztv <qi@outlook.cz>
2024-10-13 14:16:44 +0800

    pro

--- src/Cronicle_setup.sh
+++ src/Cronicle_setup.sh
@@ -8,9 +8,10 @@ cronicle_basedir=${CRONICLE_base_dir}
 
 # 安装 Cronicle
 mkdir -p ${cronicle_basedir}
+chown -R ${USER}:${USER} ${cronicle_basedir}
 cd ${cronicle_basedir}
 curl -L https://github.com/jhuckaby/Cronicle/archive/v${cronicle_version}.tar.gz | tar zxvf - --strip-components 1
-npm install
+npm install --production
 node bin/build.js dist
 
 # 设置 Cronicle 配置
@@ -26,15 +27,15 @@ cat << EOF > ${cronicle_basedir}/conf/config.json
   "data_dir": "${cronicle_basedir}/data",
   "plugins_dir": "${cronicle_basedir}/plugins",
   "pid_file": "${cronicle_basedir}/logs/cronicled.pid",
-  "secret_key": "CHANGE_THIS"
+  "secret_key": "CHANGE_THIS",
+  "Storage": {
+    "engine": "Filesystem",
+    "list_page_size": 50,
+    "concurrency": 4,
+    "base_dir": "${cronicle_basedir}/data"
+  }
 }
 EOF
-
-# 创建必要的目录
-mkdir -p ${cronicle_basedir}/{logs,data,plugins}
-
-# 设置权限
-chown -R nocodb:nocodb ${cronicle_basedir}
 
 # 初始化存储
 cd ${cronicle_basedir}
