a0bf8b852ca03580bf420ccd6846601e895bf77e
orztv <qi@outlook.cz>
2024-10-15 13:43:55 +0800

    update

--- src/remix_setup.sh
+++ src/remix_setup.sh
@@ -3,8 +3,8 @@ set -e
 
 echo "安装、配置 Remix 开始"
 
-# 全局安装 pnpm 和 pm2
-npm install -g pnpm
+# 全局安装 pnpm 和 remix-cli
+npm install -g pnpm @remix-run/cli
 
 # 克隆项目
 git clone -b main https://github.com/aigem/smartcode.git /usr/src/app/smartcode
@@ -12,7 +12,12 @@ git clone -b main https://github.com/aigem/smartcode.git /usr/src/app/smartcode
 # 设置目录权限
 chown -R ${USER}:${USER} /usr/src/app/smartcode
 
-# 安装依赖并构建项目
-cd /usr/src/app/smartcode && pnpm install && pnpm build
+cd /usr/src/app/smartcode
+
+# 使用 pnpm 安装依赖
+pnpm install --production
+
+# 构建项目
+NODE_ENV=production pnpm run build
 
 echo "Remix 安装和初始化完成"
