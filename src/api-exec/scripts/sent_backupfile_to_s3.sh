d043addb5f64a60f87fea48220b183644566a775
orztv <qi@outlook.cz>
2024-10-22 14:26:03 +0800

    update

--- src/api-exec/scripts/sent_backupfile_to_s3.sh
+++ src/api-exec/scripts/sent_backupfile_to_s3.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # 设置rclone路径
-RCLONE_PATH="$HOME/rclone/rclone"
-BACKUP_DIR="/home/nocodb/backups"
+RCLONE_PATH="$HOME_DIR/rclone/rclone"
+BACKUP_DIR="$HOME_DIR/backups"
 
 # 检查rclone是否存在
 if [ -f "$RCLONE_PATH" ]; then
@@ -16,7 +16,6 @@ fi
 # 检查备份文件是否存在
 if [ -z "$BACKUP_FILE" ]; then
     # 如果BACKUP_FILE未定义,使用backup_sql_to_local.sh中的定义
-    BACKUP_DIR="/home/nocodb/backups"
     BACKUP_FILE="$BACKUP_DIR/nocodb_backup_dump.sql"
 fi
 
@@ -28,8 +27,6 @@ if [ ! -f "$BACKUP_FILE" ]; then
 fi
 
 # 使用rclone上传文件到S3兼容的存储服务
-echo "尝试列出存储桶内容..."
-$RCLONE_PATH ls r2s3:noco-db
 echo "开始上传备份文件到S3兼容的存储服务..."
 $RCLONE_PATH copy "$BACKUP_DIR" r2s3:noco-db/nocodb-backups/ -vv
 
