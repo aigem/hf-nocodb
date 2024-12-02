4940a8308e4c92aed154bebf93c49061764b3a52
orztv <qi@outlook.cz>
2024-12-02 20:52:38 +0800

    add remote pgsql

--- src/nocodb_setup.sh
+++ src/nocodb_setup.sh
@@ -7,7 +7,5 @@ echo "export NC_AUTH_JWT_SECRET=nocodb_jwt_secret" >> $HOME_DIR/.nocodb_env
 echo "export NC_TOOL_DIR=/usr/app/data/" >> $HOME_DIR/.nocodb_env
 echo "export NC_ALLOW_LOCAL_HOOKS=true" >> $HOME_DIR/.nocodb_env
 echo "export NC_REDIS_URL=redis://:redis_password@localhost:6379/4" >> $HOME_DIR/.nocodb_env
-echo "export NC_PUBLIC_URL=/" >> $HOME_DIR/.nocodb_env
-echo "export PORT=7861" >> $HOME_DIR/.nocodb_env
 
 chmod 644 $HOME_DIR/.nocodb_env
