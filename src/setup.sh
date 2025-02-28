2a7c8fb3b97b0acd205ebbe9f9dc548bfc92019e
orztv <qi@outlook.cz>
2025-02-28 21:03:15 +0800

    update

--- src/setup.sh
+++ src/setup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
-# 安装软件包
-apt-get update && apt-get install -y dasel dumb-init nodejs npm curl python3 python3-pip
 
 cd $HOME_DIR
+pwd
 curl http://get.nocodb.com/linux-x64 -o nocodb -L && chmod +x nocodb
+ls
 
 echo "NocoDB 安装初始化完成"
